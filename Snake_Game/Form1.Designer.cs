4dcc9e2e2976db3c28e82afb40d5ee652d8c821a
Guilherme <guilhermegv890@gmail.com>
2022-09-09 11:01:55 -0300

    First Game Snake C#

---
+++ Snake_Game/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Snake_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbScore = new System.Windows.Forms.Label();
+            this.lbFood = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbScore
+            // 
+            this.lbScore.AutoSize = true;
+            this.lbScore.Location = new System.Drawing.Point(0, 0);
+            this.lbScore.Name = "lbScore";
+            this.lbScore.Size = new System.Drawing.Size(124, 27);
+            this.lbScore.TabIndex = 0;
+            this.lbScore.Text = "Score: 0";
+            // 
+            // lbFood
+            // 
+            this.lbFood.AutoSize = true;
+            this.lbFood.BackColor = System.Drawing.Color.Red;
+            this.lbFood.Location = new System.Drawing.Point(625, 250);
+            this.lbFood.Name = "lbFood";
+            this.lbFood.Size = new System.Drawing.Size(26, 27);
+            this.lbFood.TabIndex = 1;
+            this.lbFood.Tag = " ";
+            this.lbFood.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.Controls.Add(this.lbFood);
+            this.Controls.Add(this.lbScore);
+            this.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbScore;
+        private System.Windows.Forms.Label lbFood;
+    }
+}
+
---
+++ Snake_Game/Piece.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake_Game
+{
+    class Piece: System.Windows.Forms.Label
+    {
+        public Piece(int x,int y)
+        {
+
+            Location = new System.Drawing.Point(x, y);
+            Size = new System.Drawing.Size(21, 21);
+            BackColor = System.Drawing.Color.Orange;
+            Enabled = false;
+        }
+    }
+}
